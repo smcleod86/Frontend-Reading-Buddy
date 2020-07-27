ef4b43f343859a56f2752bfd6c498f80c4e55b0c
Emily Krauss <e.krauss27@gmail.com>
2020-07-26 21:23:29 -0700

    created form elements on Login.js, needs functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
+
+
 export default function Login() {
     return (
-        <>
-            <h1>Login to Reading Buddy</h1>
-        </>
+        <div>
+            <form>
+                <label>
+                    User Name or Email:
+                    <input type="text" name="user" />
+                </label>
+                <br></br>
+                <label>
+                    Password:
+                    <input type="password" name="password" />
+                </label>
+                <br></br>
+                <input type="submit" value="Submit" />
+                <button>Clear</button>
+
+            </form>
+            <p>Sign-up</p>
+        </div>
     )
 }
