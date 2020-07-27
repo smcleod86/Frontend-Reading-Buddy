794dae4631b076cc601f79a7d47dc9838cc5c186
Emily Krauss <e.krauss27@gmail.com>
2020-07-27 11:37:39 -0700

    boilerplate for profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,15 @@
-  
 import React from 'react'
 
+
 export default function Profile() {
+
     return (
-        <>
-            <h1>Profile</h1>
-        </>
+        <div>
+            <p>Number of books read this week: </p>
+            <p>Number of books read this month: </p>
+            <button>See my Friends List</button> <br></br>
+            <button>See my Reviews</button> <br></br>
+            <button>Want to read</button> <button>Currently reading</button> <button>Have read</button>
+        </div>
     )
 }
