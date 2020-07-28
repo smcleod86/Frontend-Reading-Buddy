d42b837472d833fcce0b67c1844f40c6f94bab29
Eliott Davidson <ulthos@gmail.com>
2020-07-28 10:52:37 -0700

    UserExperience component's form values now update local state when changed

--- src/components/UserExperience.js
+++ src/components/UserExperience.js
@@ -2,12 +2,25 @@ import React , { useState } from 'react'
 
 export default function UserExperience({bookInfo, userExperienceInfo}) {
     const ratingOptions = ["1", "2", "3", "4", "5"];
-    let [ rating, setFormRating ] = useState(userExperienceInfo.rating);
-    let [ review, setFormReview ] = useState(userExperienceInfo.review);
-    let [ dateStarted, setFormDateStarted ] = useState(userExperienceInfo.date_started);
-    let [ dateFinished, setFormDateFinsihed ] = useState(userExperienceInfo.date_finished);
+    let [ rating, setRating ] = useState(userExperienceInfo.rating);
+    let [ review, setReview ] = useState(userExperienceInfo.review);
+    let [ dateStarted, setDateStarted ] = useState(userExperienceInfo.date_started);
+    let [ dateFinished, setDateFinished ] = useState(userExperienceInfo.date_finished);
     console.log(`rating: ${rating}, props: ${rating}`);
     console.log(`dateStarted: ${dateStarted}`)
+
+    const handleRating = (e) => {
+        setRating(e.target.value)
+    }
+    const handleReview = (e) => {
+        setReview(e.target.value)
+    }
+    const handleDateStarted = (e) => {
+        setDateStarted(e.target.value)
+    }
+    const handleDateFinished = (e) => {
+        setDateFinished(e.target.value)
+    }
 
     return (
         <>
@@ -21,17 +34,17 @@ export default function UserExperience({bookInfo, userExperienceInfo}) {
             <div className="container right-panel">
                 <form>
                     <label htmlFor="rating">Rating:</label>
-                    <select id="rating" defaultValue={rating} name="rating">
+                    <select id="rating"  name="rating" defaultValue={rating} onChange={handleRating}>
                         { ratingOptions.map(ratingOption => {
                                 return <option key={ratingOption} value={ratingOption}>{ratingOption}</option>
                         })}
                     </select>
                     <label htmlFor="review">Review:</label>
-                    <input type="text" id="review" name="review" defaultValue={review}/>
+                    <input type="text" id="review" name="review" defaultValue={review} onChange={handleReview} />
                     <label htmlFor="dateStarted">Date started:</label>
-                    <input type="date" id="dateStarted" defaultValue={dateStarted}/>
+                    <input type="date" id="dateStarted" defaultValue={dateStarted} onChange={handleDateStarted} />
                     <label htmlFor="dateFinished">Date finished:</label>
-                    <input type="date" id="dateFinished" defaultValue={dateFinished}/>
+                    <input type="date" id="dateFinished" defaultValue={dateFinished} onChange={handleDateFinished}/>
                     <input type="submit" value="Submit" />
                 </form>
             </div>
