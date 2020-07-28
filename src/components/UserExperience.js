81dd64e6609b4f68dc52d25036838a50821872d8
Eliott Davidson <ulthos@gmail.com>
2020-07-28 10:45:33 -0700

    UserExperience component now creates a local useState for tracking update form entries

--- src/components/UserExperience.js
+++ src/components/UserExperience.js
@@ -1,30 +1,37 @@
-import React from 'react'
+import React , { useState } from 'react'
 
-export default function UserExperience(props) {
+export default function UserExperience({bookInfo, userExperienceInfo}) {
     const ratingOptions = ["1", "2", "3", "4", "5"];
+    let [ rating, setFormRating ] = useState(userExperienceInfo.rating);
+    let [ review, setFormReview ] = useState(userExperienceInfo.review);
+    let [ dateStarted, setFormDateStarted ] = useState(userExperienceInfo.date_started);
+    let [ dateFinished, setFormDateFinsihed ] = useState(userExperienceInfo.date_finished);
+    console.log(`rating: ${rating}, props: ${rating}`);
+    console.log(`dateStarted: ${dateStarted}`)
+
     return (
         <>
             <div className="container left-panel">
                 <h3>Current Book:</h3>
-                <h4>{props.bookInfo.title}</h4>
-                <p>Author: {props.bookInfo.author}</p>
-                <p>Genre: {props.bookInfo.genre}</p>
-                <p>Summary: {props.bookInfo.summary}</p>
+                <h4>{bookInfo.title}</h4>
+                <p>Author: {bookInfo.author}</p>
+                <p>Genre: {bookInfo.genre}</p>
+                <p>Summary: {bookInfo.summary}</p>
             </div>
             <div className="container right-panel">
                 <form>
-                    <label for="rating">Rating:</label>
-                    <select id="rating" defaultValue={props.userExperienceInfo.rating} name="rating">
+                    <label htmlFor="rating">Rating:</label>
+                    <select id="rating" defaultValue={rating} name="rating">
                         { ratingOptions.map(ratingOption => {
                                 return <option key={ratingOption} value={ratingOption}>{ratingOption}</option>
                         })}
                     </select>
-                    <label for="review">Review:</label>
-                    <input type="text" id="review" name="review" defaultValue={props.userExperienceInfo.review}/>
-                    <label for="date_started">Date started:</label>
-                    <input type="date" id="date_started" defaultValue={props.userExperienceInfo.date_started}/>
-                    <label for="date_finished">Date finished:</label>
-                    <input type="date" id="date_finished" defaultValue={props.userExperienceInfo.date_finished}/>
+                    <label htmlFor="review">Review:</label>
+                    <input type="text" id="review" name="review" defaultValue={review}/>
+                    <label htmlFor="dateStarted">Date started:</label>
+                    <input type="date" id="dateStarted" defaultValue={dateStarted}/>
+                    <label htmlFor="dateFinished">Date finished:</label>
+                    <input type="date" id="dateFinished" defaultValue={dateFinished}/>
                     <input type="submit" value="Submit" />
                 </form>
             </div>
