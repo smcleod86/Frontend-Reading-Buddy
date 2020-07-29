554d9053c9a2ad17eb0470e6ce958286d7cbad0f
Eliott Davidson <ulthos@gmail.com>
2020-07-29 10:47:33 -0700

    Add Friend button now calls a user update db call.  that user route still needs to be created though

--- src/components/users_components/UserRow.js
+++ src/components/users_components/UserRow.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import Axios from 'axios'
 import { NavLink } from 'react-router-dom'
 
 export default function UserRow(props) {
-    let buttonCaption = props.friend ? "Remove" : "Add";  // in case we want to change butt
+    const handleClick = () => {
+        // TODO: HOW TO ACCESS USER ID?
+        /*
+        Axios.get(`/users/${}/update`, {friends: props.user.id})
+            .then(updateResult => {
+                //something
+            })
+            .catch(err => {
+                console.log(`Error adding friend: ${err}`);
+            })
+        */
+       console.log(`Clicked on Add Friend for ${props.user.name}`);
+    }
     return(
         <div>
-            <NavLink class="nav-link" to={`/profile/${props.user.id}`}>{props.user.name}</NavLink>
-            <button>{buttonCaption}</button>
+            <NavLink className="nav-link" to={`/profile/${props.user.id}`}>{props.user.name}</NavLink>
+            <button onClick={handleClick}>Add Friend</button>
         </div>
     )
 }
