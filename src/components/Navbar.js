e4d407b8ca6f3e6bfacf93622a470b960a465734
Emily Krauss <e.krauss27@gmail.com>
2020-07-29 14:31:13 -0700

    auth is working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,19 +14,26 @@ const Navbar = (props) => {
                         <li className="nav-item">
                             <NavLink className="nav-link" to = "/books" >Books</NavLink>
                         </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to = "/profile">Profile</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to = "/login">Login</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to = "/register">Register</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to = "/editblahblah">Working On This</NavLink>
-                        </li>
                     </ul>
+                    {
+                        props.isAuthed
+                        ? <ul className="nav-items">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to = "/profile">Profile</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <span onClick={props.handleLogout} className="nav-link logout-link" to="/login">Logout</span>
+                            </li>
+                         </ul>
+                        : <ul className="nav-items">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to = "/register">Register</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/login">Login</NavLink>
+                            </li>
+                         </ul>
+                    }
                 </div>
             </div>
         </nav>
