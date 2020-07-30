32d43a2c44a429b2ec3920cc60215892e4a456a1
Eliott Davidson <ulthos@gmail.com>
2020-07-29 18:20:58 -0700

    Finished FindFriends page.  User can search for other users by first and/or last name.  Search results appear with an Add-Friend or Remove-Friend button next to them, depending on whether they are already friends of the user.  Both types of buttons work.  They also update immediately when clicked, so for instance the Add Friend button will turn into a Remove Friend button after it is clicked.  The user themself is also excluded from the search results.  We hope you're your own best friend, but you don't need to be that on the app.

--- src/components/FindFriends.js
+++ src/components/FindFriends.js
@@ -3,9 +3,8 @@ import UserRow from './users_components/UserRow.js'
 import Axios from 'axios';
 
 export default function FindFriends(props) {
-    let sampleData = [{first_name: "bob", last_name: "brown", id:  "0294r2r"}, {first_name: "Erika", last_name: "Svensworth", id: "2490fds"}];
-    let myFriends = [{name: "Erika", id: "2490fds"}];
-    let [userList, setUserList ] = useState(sampleData);
+    //let sampleData = [{first_name: "bob", last_name: "brown", id:  "0294r2r", key:"0294r2r"}, {first_name: "Erika", last_name: "Svensworth", id: "2490fds", key: "2490fds"}];
+    let [ userList, setUserList ] = useState([]);
     let [ firstName, setFirstName ] = useState("");
     let [ lastName, setLastName ] = useState("");
 
@@ -26,9 +25,25 @@ export default function FindFriends(props) {
         }
         if (query) {
             console.log(`Sending axios call to ${process.env.REACT_APP_SERVER_URL}/users?${query}`)
+            // find all users whose names match the query.  Then compare them to the user's friend list so we can put appropriate add/remove friend buttons by them
+            // also remove the user from the search results entirely, if they happen to search for their own name
             Axios.get(`${process.env.REACT_APP_SERVER_URL}/users?${query}`)
-                .then(response => {
-                    setUserList(response.data.searchResults);
+                .then(queryResponse => {
+                    Axios.get(`${process.env.REACT_APP_SERVER_URL}/users/${props.currentUser.id}`)
+                    .then(friendListResponse => {
+                        let myFriends = friendListResponse.data.user.friends;
+                        queryResponse.data.searchResults.forEach((user, index) => {
+                            if (myFriends.includes(user._id)){
+                                user.isFriend = true;
+                            } else if (user._id === props.currentUser.id){
+                                queryResponse.data.searchResults.splice(index, 1)
+                            }
+                        })
+                        setUserList(queryResponse.data.searchResults);
+                    })
+                    .catch(err => {
+                        console.log(`Error retrieving existing friend list: ${err}`)
+                    })
                 })
                 .catch(err => {
                     console.log(`Error getting search results: ${err}`);
@@ -36,16 +51,6 @@ export default function FindFriends(props) {
         }
     }
 
-    //Needs currentUser.id passed as a prop
-    /* 
-    Axios.get('/users/${props.currentUser.id')
-        .then(userData => {
-            let myFriends.concat(userData.friends);
-        })
-        .catch(err => {
-            console.log(`Error retrieving existing friend list: ${err}`)
-        })
-    */
 
 
     return(
@@ -53,7 +58,7 @@ export default function FindFriends(props) {
             <div>
                 <h2> Search Results </h2>
                 {userList.map(user => {
-                    return <UserRow user={user} friend={true} key={user.id} />
+                    return <UserRow currentUser={props.currentUser} user={user} key={user._id} />
                 })}
             </div>
             <div>
